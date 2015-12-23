b3ea643b8f3206299187f6baaf976158c3959073
Matthew Zehnder <mz@uber.com>
2015-12-23 03:16:32 -0800

    Adding github-notes

---
+++ github-notetaker/app/components/Github/Repos.js
@@ -0,0 +1,30 @@
+var React = require('react')
+
+var Repos = React.createClass({
+
+	propTypes: {
+
+		username: React.PropTypes.string.isRequired,
+		repos: React.PropTypes.array.isRequired,
+
+		},
+
+	render: function(){
+
+		console.log(this.props.repos)
+
+		return(
+
+			<div> 
+
+			<p>Repos</p>
+
+
+
+			</div>
+
+		)
+	}
+});
+
+module.exports = Repos;
---
+++ github-notetaker/app/components/Github/UserProfile.js
@@ -0,0 +1,29 @@
+var React = require('react')
+
+var UserProfiles = React.createClass({
+
+	propTypes: {
+
+	username: React.PropTypes.string.isRequired,
+	bio: React.PropTypes.object.isRequired,
+
+	},
+
+	render: function(){
+
+		console.log(this.props.bio)
+
+		return(
+
+			<div> 
+
+			<p>User Profile</p>
+			<p>Username: {this.props.username}</p>
+
+			</div>
+
+		)
+	}
+});
+
+module.exports = UserProfiles;
