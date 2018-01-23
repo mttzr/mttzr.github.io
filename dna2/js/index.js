42ef30d0265b622b0373409c78be3ab5e8b8ce1a
Matthew Zehnder <matthewzehnder@big-m.lan>
2018-01-22 23:49:01 -0800

    blurb spacing

--- dna2/js/index.js
+++ dna2/js/index.js
@@ -106,12 +106,38 @@ function refresh() {
 function moveDown()
 {
 	var elArray = ['bp1','bp2','bp3','bp4','bp5','bp6','bp7','bp8'];
-	var itr = 8;
+	var itr = 7;
 
-	while (itr < 8)
+	while (itr > 1)
 	{
 		var current = document.getElementById(elArray[itr]);
-		var previous = document.getElementById(elArray[itr]);
+		var currentChild1 = current.childNodes[0];
+		current.removeChild(currentChild1);
+		var currentChild2 = current.childNodes[0];
+		current.removeChild(currentChild2);
+		var previous = document.getElementById(elArray[itr-1]);
+		console.log(previous);
+		var previousChild1 = previous.childNodes[0];
+		previous.removeChild(previousChild1);
+		var previousChild2 = previous.childNodes[0];
+		previous.removeChild(previousChild2);
+
+		while (current.firstChild)
+		{
+			current.removeChild(current.firstChild);
+		}
+
+		current.appendChild(previousChild1);
+		current.appendChild(previousChild2)
+		itr-1;
+	}
+	if (itr == 0)
+	{
+		var current = document.getElementById(elArray[itr]);
+		var newSpan = randomBp();
+		current.appendChild(newSpan);
+		var newSpan = randomBp();
+		current.appendChild(newSpan);	
 	}
 
 }
