f2eddf0aa14dc9c7f4a64c279a8d632f889a79f8
Matthew Zehnder <zehnder.matt@gmail.com>
2020-11-29 11:55:14 -0800

    updated logos and style fixes

--- js/script.js
+++ js/script.js
@@ -11,12 +11,10 @@ class NavComponent extends HTMLElement {
 		<div class="container"> \
     		<div class="row" style="margin-top: 5%">\
       			<div class="six columns">\
-        		<img class="logo" src="./images/ReturntoZehnder'+logo+'.png">\
+        		<img class="logo" src="./images/Return to Zehnder-'+logo+'.png">\
       			</div>\
       			<div class="six columns nav__links">\
 		        <a href="index.html" class = "nav__link hvr-grow">home</a>\
-		        <a href="writing.html" class = "nav__link hvr-grow">writing</a>\
-		        <a href="reading.html" class = "nav__link hvr-grow">reading</a>\
 		        <a href="about.html" class = "nav__link hvr-grow">about</a>\
 		        <a href="contact.html" class = "nav__link hvr-grow">contact</a>\
       			</div>\
@@ -32,3 +30,11 @@ const article = document.querySelector('#electric-cars');
 console.log("hey")
 
 console.log(article)
+
+/*
+Add new pages later
+		        <a href="writing.html" class = "nav__link hvr-grow">writing</a>\
+		        <a href="reading.html" class = "nav__link hvr-grow">reading</a>\
+
+
+*/
